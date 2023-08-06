bd6c2e31a933426e0db8975a9b8cea82d22c4f7c
jackrichter <jackrichter1@gmail.com>
2023-08-06 03:01:23 +0200

    Numbers: Digit extraction and sum of the even or the odd digits

---
+++ src/main/java/com/barath/javacode/numbers/DigitExtractorOddDigits.java
@@ -0,0 +1,26 @@
+package com.barath.javacode.numbers;
+
+import java.util.Scanner;
+
+public class DigitExtractorOddDigits {
+    public static void main(String[] args) {
+        System.out.println("Enter a number greater than zero:");
+        Scanner scan = new Scanner(System.in);
+        int num = scan.nextInt();
+        int digit = 0;
+        int sumOddDigits = 0;
+
+        while(num != 0) {
+            digit = num % 10;
+            System.out.print(digit + " ");
+            num = num / 10;
+            if (digit % 2 != 0) {
+                sumOddDigits = sumOddDigits + digit;
+            }
+        }
+        System.out.println();
+        System.out.println("The sum of the even digits is: " + sumOddDigits);
+
+        scan.close();
+    }
+}
